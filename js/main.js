54a9be2b1989104f5bf68aeebc45674d4e1662a6
Patrick Willians Powel <61129859+PatrickPowel@users.noreply.github.com>
2022-08-24 14:20:20 -0300

    Layout Marketing Digital
    Layout de Marketing Digital Modelo Animação Simples

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function typeWriter(elemento) {
+    var textArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textArray.forEach((letra, i) => {
+        setTimeout(() => {
+            elemento.innerHTML += letra;
+        }, 75 * i);
+    });
+}
+
+var titulo = document.querySelector('h1');
+typeWriter(titulo);
+var titulo = document.querySelector('#msg2');
+typeWriter(titulo);
